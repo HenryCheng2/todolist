b5d2ad41dee8b69279c804680df82267dd9aa168
HenryCheng2 <henrycheng374@gmail.com>
2022-11-26 19:46:07 -0500

    added a page for a different list

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 //require
 const app = express();
 
-var items=[ "buy food","cook food","eat food"];
+let items=[ "buy food","cook food","eat food"];
+let workItems=[];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,23 +22,37 @@ app.get("/", function (req, res) {
   
   var day =today.toLocaleDateString("en-US", options); //convets varible today to a string for current date. 
 
-  res.render("list", { kindOfDay: day, newListItem:items });
-  // renders the list.ejs file as a webpage and passes the variable 'day' into the kindOfDay variable listed between the <%= EJS %> marker.
+  res.render("list", { ListTitle: day, newListItem:items });
+  // renders the list.ejs file as a webpage and passes the variable 'day' into the ListTitle variable listed between the <%= EJS %> marker.
 });
 
 // handle post request from list.ejs
 app.post("/", function(req, res){
   var item = req.body.newItem;
+  
   if(item == ""){
     console.log("blank input");
     res.redirect("/");
     return 0;
-
   }
-  items.push(item);
-  res.redirect("/");
+
+  if(req.body.list==="Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+
 
 });
+
+// takes the template from the "/" page and uses it to make the work page
+app.get("/work",function(req, res){
+  res.render("list",{ListTitle: "Work List", newListItem:workItems});
+});
+
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
